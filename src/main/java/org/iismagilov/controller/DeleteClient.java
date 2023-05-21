ff3158068c49089f2c6940da4342616826ad1452
AliasH315 <isma9iloff@yandex.ru>
2023-05-22 02:01:43 +0300

    Delete Client is complete!

--- src/main/java/org/iismagilov/controller/DeleteClient.java
+++ src/main/java/org/iismagilov/controller/DeleteClient.java
@@ -8,7 +8,6 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.iismagilov.model.QueryDAO;
